f2967ffd6e372f3432c610be9e80f150e51e8b4c
IgorZaitsew <43369437+IgorZaitsew@users.noreply.github.com>
2019-11-19 12:33:45 +0300

    v1.0

--- NumberConverter/src/test/java/by/zaycevigor/service/NumberServiceTest.java
+++ NumberConverter/src/test/java/by/zaycevigor/service/NumberServiceTest.java
@@ -1,6 +1,7 @@
 package by.zaycevigor.service;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
@@ -10,18 +11,16 @@ import static by.zaycevigor.service.NumberService.SERVICE;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberServiceTest {
-    public static final String INPUT_NUMBER = "Input a number: ";
-    public static final String INPUT_RESULT = "Input the expected result: ";
-    public static final String OUTPUT_TEXT = "Your number: %d \n";
+    static String[][] numbers = {{"123", "сто двадцать три"},
+                {"951051001921", "девятьсот пятьдесят один миллиард пятьдесят один миллион одна тысяча девятьсот двадцать один"},
+                {"000001", "один"},
+                {"2", "два"}};
+
 
     @Test
     void convertTest() throws NumberException {
-        String[][] numbers = {{"123", "сто двадцать три"},
-                {"951051001921", "девятьсот пятьдесят один миллиард пятьдесят один миллион одна тысяча девятьсот двадцать один"},
-                {"000001","один"},
-                {"2","два"}};
-        for(String[] pair:numbers){
-            assertEquals(pair[1].trim(),(SERVICE.convert(pair[0]).trim()).toLowerCase());
+        for (String[] pair : numbers) {
+            assertEquals(pair[1].trim(), (SERVICE.convert(pair[0]).trim()).toLowerCase());
         }
     }
 }
